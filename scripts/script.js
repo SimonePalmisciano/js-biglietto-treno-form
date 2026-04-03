45b644131ac4cc795ea76e787e55762d95d30859
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-03 16:05:29 +0200

    creata funzione con all'interno const che mostra il prezzo del biglietto in base all'età

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,29 @@ const scontoSenior = 0.6;
 const nomePasseggero = document.querySelector('#inputNameSurname');
 const kmPasseggero = document.querySelector('#inputKm');
 const etaPasseggero = document.querySelector('#selectAge');
+const bottoneConferma = document.querySelector('#btnConferma');
+const bottoneAnnulla = document.querySelector('#btnAnnulla');
 
-function calcolaPrezzoBiglietto () {
-    
+function calcolaPrezzoBiglietto(event) {
+    event.preventDefault();
+
+    const nomePasseggeroValue = nomePasseggero.value;
+    const kmPasseggeroValue = kmPasseggero.value;
+    const etaPasseggeroValue = etaPasseggero.value;
+    const prezzoBiglietto = kmPasseggeroValue * prezzoKm;
+
+    if (etaPasseggeroValue === "underage") {
+        console.log(`prezzo scontato Junior ${prezzoBiglietto * scontoJunior}`);
+        
+
+    } else if (etaPasseggeroValue === "adult") {
+        console.log(`prezzo normale ${prezzoBiglietto}`);
+        
+    } else if (etaPasseggeroValue === "over65") {
+        console.log(`prezzo scontato Senior ${prezzoBiglietto * scontoSenior}`);
+        
+    }
+
 }
+
+bottoneConferma.addEventListener('click', calcolaPrezzoBiglietto);
