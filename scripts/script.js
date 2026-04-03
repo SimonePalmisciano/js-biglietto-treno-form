8284d5e6b70d148227c92855af774a65edd83845
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-03 15:45:33 +0200

    create le variabili globali

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const prezzoKm = 0.21;
+const scontoJunior = 0.8;
+const scontoSenior = 0.6;
+const nomePasseggero = document.querySelector('#inputNameSurname');
+const kmPasseggero = document.querySelector('#inputKm');
+const etaPasseggero = document.querySelector('#selectAge');
+
+function calcolaPrezzoBiglietto () {
+    
+}
